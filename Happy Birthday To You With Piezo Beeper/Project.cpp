120de4cf795119dedf8782aeca0af12fc02df9cc
GeoSn0w <office@fce365.info>
2026-01-11 20:07:43 +0200

    Create Project.cpp

---
+++ Happy Birthday To You With Piezo Beeper/Project.cpp
@@ -0,0 +1,36 @@
+int piezo = 8;
+
+void setup() {
+  pinMode(piezo, OUTPUT);
+}
+
+void loop() {
+  beep(262, 180); beep(262, 180);
+  beep(294, 360); beep(262, 360);
+  beep(349, 360); beep(330, 720);
+  delay(500);
+  
+  beep(262, 180); beep(262, 180);
+  beep(294, 360); beep(262, 360);
+  beep(392, 360); beep(349, 720);
+  delay(500);
+  
+  beep(262, 180); beep(262, 180);
+  beep(523, 360); beep(440, 360);
+  beep(349, 360); beep(330, 360);
+  beep(294, 720);
+  delay(500);
+  
+  beep(466, 180); beep(466, 180);
+  beep(440, 360); beep(349, 360);
+  beep(392, 360); beep(349, 720);
+  
+  delay(2500);
+}
+
+void beep(int frequency, int duration) {
+  tone(piezo, frequency);
+  delay(duration * 0.7);
+  noTone(piezo);
+  delay(duration * 0.3);
+}
